a5fdefc3916f8f2dd2a99fade2b90ba6f18cb868
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-03-27 17:53:53 -0300

    Feito a conexão com a API.

--- src/main/java/br/com/alura/projetoBuscaFipe/ProjetoBuscaFipeApplication.java
+++ src/main/java/br/com/alura/projetoBuscaFipe/ProjetoBuscaFipeApplication.java
@@ -1,13 +1,22 @@
 package br.com.alura.projetoBuscaFipe;
 
+import br.com.alura.projetoBuscaFipe.servicos.ConsumindoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoBuscaFipeApplication {
+public class ProjetoBuscaFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoBuscaFipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumindoApi consumindoApi = new ConsumindoApi();
+
+		System.out.println(consumindoApi.conectandoApi());
+
+	}
 }
