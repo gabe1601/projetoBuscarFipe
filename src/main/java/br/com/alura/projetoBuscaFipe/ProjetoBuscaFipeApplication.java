5108fe2f3ee24b2ed5d092e192f6890324b88782
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-03-27 17:31:34 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/alura/projetoBuscaFipe/ProjetoBuscaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.projetoBuscaFipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBuscaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBuscaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/projetoBuscaFipe/ProjetoBuscaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.projetoBuscaFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBuscaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
