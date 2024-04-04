e94c2b547638f47ba967c1f88a289b2104aa23b6
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-04-04 17:35:07 -0300

    Convertendo a API para lista.

--- src/main/java/br/com/alura/projetoBuscaFipe/servicos/ConverterDados.java
+++ src/main/java/br/com/alura/projetoBuscaFipe/servicos/ConverterDados.java
@@ -1,28 +1,33 @@
 package br.com.alura.projetoBuscaFipe.servicos;
 
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import java.util.List;
 
 public class ConverterDados implements IConverteDados {
 
-    private ObjectMapper mapper = new ObjectMapper();
-
+    ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
         try {
-            return mapper.readValue(json,classe);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            return mapper.readValue(json, classe);
+        }catch (JsonProcessingException e){
+            throw  new RuntimeException();
         }
     }
 
     @Override
     public <T> List<T> obterLista(String json, Class<T> classe) {
-        return null;
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe);
+        try{
+           return mapper.readValue(json, lista);
+        }catch (JsonProcessingException e){
+            throw new RuntimeException();
+        }
     }
-
-
 }
